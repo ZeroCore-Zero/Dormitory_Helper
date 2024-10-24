bc942195ae9267ff426670d0b9aa813f65f7dfe8
Iamnotzzz <zzz2638626195@outlook.com>
2024-10-24 17:16:16 +0800

    front change

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,10 +1,14 @@
 <template>
-    <table>
+    <el-table :data="tableData" border stripe >
         <caption>Cookies View</caption>
-        <thead>
+        <el-table-column prop="name" label="Name" width="180"/>
+        <el-table-column prop="value" label="value" width="280"/>
+
+        <!-- 原版代码 -->
+        <!-- <thead> 
             <tr>
                 <th>Name</th>
-                <th>Value</th>
+                <th>Value</th>  
             </tr>
         </thead>
         <tbody>
@@ -12,13 +16,15 @@
                 <td>{{ name }}</td>
                 <td>{{ value }}</td>
             </tr>
-        </tbody>
-    </table>
+        </tbody> -->
+    </el-table>
     <RouterView />
 </template>
 
 <script setup>
+import { ElTable, ElTableColumn } from 'element-plus';
 import { useCookies } from "@vueuse/integrations/useCookies"
 const cks = useCookies();
-// cks.set("token", "testtoken");
+cks.set("token", "testtoken");
+const tableData = [{name: "token2", value: "testtoken2"},{name: "token1", value: "testtoken1"}]
 </script>
--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,8 +1,14 @@
 <template>
     <el-container>
+
         <el-header>
-            Dormitory_Helper
+            <div>
+                <img src="../../photo/logo.png" width="56" height="56">
+                <!-- 这里是校徽图片，网页中的header的width为56.6 -->
+                <span>电商后台管理系统</span>
+            </div>
         </el-header>
+
         <el-main>
             <el-tabs tab-position="left" model-value="name1">
                 <el-tab-pane label="AA统计" name="name1">
@@ -11,6 +17,7 @@
                 <el-tab-pane label="电费监测" name="name2">正在开发</el-tab-pane>
             </el-tabs>
         </el-main>
+
     </el-container>
 </template>
 
@@ -24,4 +31,24 @@
     word-break: break-all;
     word-wrap: break-word;
 }
+
+/* 以下均为对于导航栏的修饰，使用了div的嵌套 */
+.el-header{
+background-color: #c8e8f5;
+display: flex;
+color: #1860e6;
+font-size: 25px;
+padding-left: 0;
+
+div{
+display: flex;
+align-items: center;
+span
+{
+  margin-left: 15px;
+}
+}
+
+}
+
 </style>
