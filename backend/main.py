32d8741b4cec23afa819a8ceff6d462a6e4a736a
ZeroCore <esetting@hotmail.com>
2025-02-15 04:32:22 +0800

    move mysql config info from python file to a separated toml file

--- backend/main.py
+++ backend/main.py
@@ -1,10 +1,14 @@
 from flask import Flask
+from tomllib import load
 from app import auth
 from app.db import db
 
+with open("./config.toml", "rb") as file:
+    config = load(file)
+
 # Init app
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://BUPT:mysql-bupt-pswd@localhost/BUPT_Helper'
+app.config['SQLALCHEMY_DATABASE_URI'] = f"mysql+pymysql://{config['mysql']['username']}:{config['mysql']['password']}@{config['mysql']['address']}/{config['mysql']['database']}"
 app.register_blueprint(auth.login)
 
 db.init_app(app)
